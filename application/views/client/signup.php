bf40136939941fddaa7000ed752bc173129fb8be
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-08-20 19:49:43 +0700

    update

--- application/views/client/signup.php
+++ application/views/client/signup.php
@@ -6,7 +6,7 @@
       name="viewport"
       content="width=device-width, initial-scale=1, minimum-scale=1, minimal-ui"
     />
-    <title>MobioKit - Premium Mobile Template</title>
+    <title>Motowash</title>
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/client/vendor/swiper/swiper.min.css" />
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/client/css/style.css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/client/css/custom.css" />
